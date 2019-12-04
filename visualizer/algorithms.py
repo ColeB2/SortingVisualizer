338a11b0ddd9ab9cadaaa1c27569dd4757d0b146
Cole <cebCole@gmail.com>
2019-12-04 11:34:40 -0600

    + Merge sort implemented, adjusted from OrangeFish implementation

--- visualizer/algorithms.py
+++ visualizer/algorithms.py
@@ -137,47 +137,87 @@ def heap_sort(array):
             j = index
             if index >= i:
                 break
+'''Merge Sort, adjust fromed code @'''
+'''https://github.com/Orangefish/algo/blob/master/sorting_and_search/sort_merge.py'''
+def merge_sort(array, l=0, u=None):
+    """
+    Merge sorting, mutable input.
+    Input Sequence changed in place.
 
+    Time: O(n * log n)
+        log n -- levels
+        n -- elements on each level must be merged
 
-'''Merge Sort'''
-def merge_sort(array):
-    if len(array) > 1:
-        mid = len(array) // 2
-        left_half = array[:mid]
-        right_half = array[mid:]
+    Space (additional): O(1)
+        input changed in place
+    Returns None
+    """
+    u = len(array) if u is None else u
+    if  u - l > 1:
+        m = l + (u - l) // 2
+        w = l + u - m
+        yield from wsort(array, l, m, w)
+        while w - l > 2:
+            n = w
+            w = l + (n - l + 1) // 2
+            yield from wsort(array, w, n, l)
+            yield from wmerge(array, l, l + n - w, n, u, w)
+        n = w
+        while n > l: # fallback to insert sort
+            for m in range(n, u):
+                if array[m-1] > array[m]:
+                    yield 0, m-1, m, array
+                    array[m-1], array[m] = array[m], array[m-1]
+                    yield 1, m-1, m, array
+            n -= 1
 
-        merge_sort(left_half)
-        merge_sort(right_half)
+def wmerge(array, i, m, j, n, w):
+    """
+    Merge subarrays [i, m) and [j, n) into work area w.
+    All indexes point into array.
+    The space after w must be enough to fit both subarrays.
+    """
+    while i < m and j < n:
+        if array[i] < array[j]:
+            yield 0, i, w, array
+            array[i], array[w] = array[w], array[i]
+            yield 1, i, w, array
+            i += 1
+        else:
+            yield 0, j, w, array
+            array[j], array[w] = array[w], array[j]
+            yield 1, j, w, array
+            j += 1
+        w += 1
+    while i < m:
+        yield 0, i, w, array
+        array[i], array[w] = array[w], array[i]
+        yield 1, i, w, array
+        i += 1
+        w += 1
+    while j < n:
+        yield 0, j, w, array
+        array[j], array[w] = array[w], array[j]
+        yield 1, j, w, array
+        j += 1
+        w += 1
 
-        i=0
-        j=0
-        k=0
+def wsort(array, l, u, w):
+    """
+    Sort subarray [l, u) and put reuslt into work area w.
+    All indexes point into array.
+    """
+    if  u - l > 1:
+        m = l + (u - l) // 2
+        yield from merge_sort(array, l, m)
+        yield from merge_sort(array, m, u)
+        yield from wmerge(array, l, m, m, u, w)
+    else:
+        while l < u:
+            yield 0, l, w, array
+            array[l], array[w] = array[w], array[l]
+            yield 1, l, w, array
+            l +=1
+            w +=1
 
-        yield 2, None, None, array
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                yield 3, k, i, j, array
-                array[k] = left_half[i]
-                yield 4, k, i, j, array
-                i = i + 1
-            else:
-                yield 3, k, j, i, array
-                array[k] = right_half[j]
-                yield 4, k, j, i, array
-                j = j + 1
-            k = k + 1
-
-        while i < len(left_half):
-            yield 3, k, i, j, array
-            array[k] = left_half[i]
-            yield 4, k, i, j, array
-            i = i + 1
-            k = k + 1
-
-        while j < len(right_half):
-            yield 3, k, j, i, array
-            array[k] = right_half[j]
-            yield 4, k, j, i, array
-            j = j + 1
-            k = k + 1
 '''Quick Sort'''
